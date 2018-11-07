c1298dfa2f2a02e77432806dc51f30de301052b7
bgshechka <bgshechka@gmail.com>
2018-11-07 19:29:42 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+const wow = require('bgshechka-test-package');
+
 const t = setInterval(() => {
-  console.log(`ping from git bot with uuid ${process.env.UUID}`);
+  console.log(`ping from git bot with uuid ${process.env.UUID}. Module bgshechka-test-package ${wow(process.env.UUID)}`);
 }, 3000);
